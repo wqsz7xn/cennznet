ac39bd7a331f11f8b93e24121bd2cba4408be4e1
wqsz7xn <wqsz7xn@protonmail.com>
2020-12-03 14:22:05 +1300

    Change to using hashes instead of passing in structs

--- crml/sylo/src/directory/mod.rs
+++ crml/sylo/src/directory/mod.rs
@@ -2,45 +2,49 @@ use frame_support::{decl_error, decl_module, decl_storage, dispatch::DispatchRes
 use frame_system::ensure_signed;
 use codec::{Decode, Encode};
 use sp_io::hashing::{keccak_256};
+use sp_core::U256;
+use sp_runtime::{DispatchError};
 
-const UNLOCK_DURATION: u32 = 100;
+// TODO: Make unlock duration setable by properly permissioned entity?
+const UNLOCK_DURATION: u32 = 1;
 
 type BalanceOf<T> = <<T as Trait>::Currency as Currency<<T as frame_system::Trait>::AccountId>>::Balance;
 type Timestamp<T> = <<T as Trait>::Time as Time>::Moment;
+type Hash = [u8; 32];
 
 #[derive(Encode, Decode, Default)]
 pub struct Stake<AccountId, Balance> {
-	amount : Balance , //amount of the stake
+	amount : Balance , // Amount of the stake
 
-    left_amount: Balance, // value of the stake on left branch
-	right_amount: Balance, // value of the stake on the right branch
+    left_amount: Balance, // Value of the stake on left branch
+	right_amount: Balance, // Value of the stake on the right branch
 	
-	stakee: AccountId,
+	stakee: AccountId, // Address of peer that offers sevices
 
-    parent: Option<[u8; 32]>, // hash of parent 
-    left : Option<[u8; 32]>, // hash of parent 
-    right : Option<[u8; 32]>, // hash of parent 
+    parent: Option<Hash>, // Hash of parent 
+    left : Option<Hash>, // Hash of left child 
+    right : Option<Hash>, // Hash of right child 
 }
 
 #[derive(Encode, Decode, Default)]
 pub struct Unlock<Balance, Timestamp> {
-	amount: Balance, // amount of stake unlocking
-    unlock_at: Timestamp, // block num stake becomes withdrawable
+	amount: Balance, // Amount of stake unlocking
+    unlock_at: Timestamp, // Block number stake becomes withdrawable
 }
 
 decl_storage! {
 	trait Store for Module<T: Trait> as SyloDirectory {
-		// Stakers
-		pub Stakes get(fn stakes): map hasher(blake2_128_concat) [u8; 32] => Stake<T::AccountId, BalanceOf<T>>;
+		// Stakers (map hash -> stake)
+		pub Stakes get(fn stakes): map hasher(blake2_128_concat) Hash => Stake<T::AccountId, BalanceOf<T>>;
 
-		// Stakees
+		// Stakees (map accountid -> balance)
 		pub Stakees get(fn stakees): map hasher(blake2_128_concat) T::AccountId => BalanceOf<T>;
 
-		// Unlockings
-		pub Unlockings get(fn unlockings): map hasher(blake2_128_concat) [u8; 32] => Unlock<BalanceOf<T>, Timestamp<T>>;
+		// Unlockings (map hash -> unlock)
+		pub Unlockings get(fn unlockings): map hasher(blake2_128_concat) Hash => Unlock<BalanceOf<T>, Timestamp<T>>;
 		
-		// Root 
-		pub Root get(fn root): [u8; 32];
+		// Root (hash)
+		pub Root get(fn root): Hash;
 	}
 }
 
@@ -61,95 +65,116 @@ decl_error! {
 		/// The address is not staking
 		NotStaking,
 		/// The unlock period has not been exhausted
-		CannotUnlock,
+		UnlockPeriodNonExhasted,
 		/// Attempt to unlock more than what is staking
 		InsufficientStakeBalance,
+		/// Nonexisting key
+		KeyDoesNotExist,
+		/// Unlock amount insuffient to the amount you're trying to relock
+		RelockAmount,
 	}
 }
 
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin, system = frame_system {
+
+		// Add a stake to the directory
 		#[weight = 0]
-		fn add_stake(origin, amount: BalanceOf<T>, stakee: T::AccountId) {
+		fn add_stake(origin, amount: BalanceOf<T>, stakee: T::AccountId) -> Result<(), DispatchError>{
 			ensure!(amount != (0 as u32).into(), Error::<T>::ZeroStake);
+
 			let staker = ensure_signed(origin)?;
 			let key = Self::get_key(staker.clone(), stakee.clone());
-			if !<Stakes<T>>::contains_key(&key) {
-				let mut parent : Option<[u8; 32]> = Some(<Root>::get());
-				let mut current : Stake<T::AccountId, BalanceOf<T>> = <Stakes<T>>::get(parent.unwrap());
-				while parent.is_some() {
+
+			// Stake does not exist, create a stake (assumption that a zero stake indiacates that it does not exist)
+			if <Stakes<T>>::get(key).amount == (0 as u32).into() {
+
+				let mut parent = Root::get();
+				
+				// Check if root (parent) exists and fetch it, otherwise None
+				let current_stake_key = parent;
+				let mut current_stake = <Stakes<T>>::get(current_stake_key);
+
+				while !(parent == [0u8; 32]) { 
 					let next;
-					if current.left_amount < current.right_amount {
-						next = current.left;
+					if current_stake.left_amount < current_stake.right_amount {
+						next = current_stake.left;
 					} else {
-						next = current.right;
+						next = current_stake.right;
 					}
 
-					if next.is_none() {
+					if next.is_none()  {
 						break;
 					}
 
-					parent = next;
-					current = <Stakes<T>>::get(next.unwrap());
+					parent = next.unwrap(); // Safe to unwrap, none check above
+					current_stake = <Stakes<T>>::get(parent);
 				}
 
-				Self::set_child(&mut current, None, Some(key));
+				Self::set_child(Some(current_stake_key), None, Some(key));
 
-				// Create a stake
-				let mut stake = Stake::<T::AccountId, BalanceOf<T>>::default();
-				stake.parent = parent;
+				// Create a stake and 
+				let mut stake = <Stakes<T>>::get(key);
+				stake.parent = Some(parent);
 				stake.stakee = stakee.clone();
-				<Stakes<T>>::insert(key, stake);
+
+				<Stakes<T>>::insert(key, stake); // insert it into directory
 
 			}
 
 			// Now that the key for the stake is defined fetch it, or if it was already defined fetch it
 			let mut stake = <Stakes<T>>::get(&key);
-			// Set the root
+
+			// Set the root if it's the first stake
 			if stake.parent.is_none() { 
-				<Root>::put(key);
+				Root::put(key);
 			}
 
-			Self::update_stake_amount(Some(key), &mut stake, amount);
+			Self::update_stake_amount(Some(key), Some(key), amount, false);
+			T::Currency::withdraw(&staker, amount, WithdrawReason::Fee.into(), ExistenceRequirement::KeepAlive)?;
 
-			T::Currency::transfer(&staker, &stakee, amount, ExistenceRequirement::KeepAlive);
+			Ok(())
 		}
 
 
 		#[weight = 0]
 		fn unlock_stake(origin, amount: BalanceOf<T>, address: T::AccountId) {
 			let staker = ensure_signed(origin)?;
-			let key = Self::get_key(staker, address);
-			let mut stake = <Stakes<T>>::get(&key);
+			let stake_key = Self::get_key(staker, address);
+			let mut stake = <Stakes<T>>::get(&stake_key);
 
 			ensure!(stake.amount > (0 as u32).into(), Error::<T>::ZeroStake);
 			ensure!(stake.amount >= amount, Error::<T>::InsufficientStakeBalance);
 
-			Self::update_stake_amount(Some(key), &mut stake, amount);
+			// Remove the balance from the stake
+			Self::update_stake_amount(Some(stake_key), Some(stake_key), amount, true);
 
 			//Stake has been withdrawn now update the tree
 			if stake.amount == (0 as u32).into() {
-				let mut child : Option<[u8; 32]>;
+				let mut child : Option<Hash>;
 				if stake.left_amount > stake.right_amount {
 					child = stake.left;
 				} else {
 					child = stake.right;
 				}
 
-				let mut parent = <Stakes<T>>::get(stake.parent.unwrap());
+				let parent_key = stake.parent;
+				let mut parent = <Stakes<T>>::get(parent_key.unwrap());
 
 				if child.is_none() {
-					Self::set_child(&mut stake, Some(key), None);
+					Self::set_child(Some(stake_key), Some(stake_key), None);
 
-					// The nly staker is removed, reset root
+					// The only staker is removed, reset root
 					if stake.parent.is_none() {
-						<Root>::set([0u8; 32]);	
+						Root::set([0u8; 32]);	
 					}
 				} else {
-					let mut current = <Stakes<T>>::get(child.unwrap());
+
+					let current_key = child.unwrap();
+					let mut current = <Stakes<T>>::get(current_key);
 
 					loop {
-						let next : Option<[u8; 32]>;
+						let next : Option<Hash>;
 						if current.left_amount > current.right_amount {
 							next = current.left;
 						} else {
@@ -164,39 +189,39 @@ decl_module! {
 					}
 
 					let current_parent = current.parent;
-					Self::set_child(&mut parent, Some(key), child);
+					Self::set_child(parent_key, Some(stake_key), child);
 					current.parent = stake.parent;
 
 
 					// update the children of current to be that of what the removed stake was
-					if current_parent.unwrap() != key {
+					if current_parent != Some(stake_key) {
 
 						// Move the children of stake to current
-						Self::fixl(&stake, child, &mut current);
-						Self::fixl(&stake, child, &mut current);
+						Self::fixl(Some(stake_key), child, child);
+						Self::fixr(Some(stake_key), child, child);
 
 						// Place stake where current was and
 						stake.parent = current_parent; // set parent
-						Self::set_child(&mut <Stakes<T>>::get(current_parent.unwrap()), current_parent, Some(key)); // set parents child
+						Self::set_child(current_parent, current_parent, Some(stake_key)); // set parents child
 
 						// Unstake amount
 						//Self::apply_stake_change(Some(key), &mut stake, current.amount.saturating_sub(current.amount), current.parent);
 
-						Self::set_child(&mut <Stakes<T>>::get(current_parent.unwrap()), Some(key), None);
+						Self::set_child(current_parent, Some(stake_key), None);
 					}
 					else if stake.left == child {
-						Self::fixr(&stake, child, &mut current);
+						Self::fixr(Some(stake_key), child, Some(current_key));
 					}
 					else {
-						Self::fixl(&stake, child, &mut current);
+						Self::fixl(Some(stake_key), child, Some(current_key));
 					}
 				}
 
 				// Now that the node is unlinked from any other nodes we can remove it.
-				<Stakes<T>>::remove(key);
+				<Stakes<T>>::remove(stake_key);
 
-				// XXX: You should be adding a unlock here
-				let mut unlock = <Unlockings<T>>::get(key);
+				// If not hit then it gives a default value for Unlock struct
+				let mut unlock = <Unlockings<T>>::get(stake_key);
 				let unlock_at = T::Time::now() + UNLOCK_DURATION.into();
 
 				if unlock.unlock_at < unlock_at {
@@ -204,146 +229,240 @@ decl_module! {
 				}
 
 				unlock.amount += amount;
+				<Unlockings<T>>::insert(stake_key, unlock);
 			}
 		}
 
-		// Reverse unlocking a certain amount of stake 
+
+		#[weight = 0]
+		fn unstake(origin, stakee: T::AccountId) {
+			let staker = ensure_signed(origin)?;
+			let key = Self::get_key(staker.clone(), stakee);
+			let unlock = <Unlockings<T>>::get(key);
+
+			ensure!(unlock.unlock_at <T::Time::now(), Error::<T>::UnlockPeriodNonExhasted);
+			ensure!(unlock.amount > (0 as u32).into(), Error::<T>::ZeroStake);
+
+			let amount = unlock.amount;
+			<Unlockings<T>>::remove(key);
+			T::Currency::deposit_into_existing(&staker, amount);
+		}
+		
+
+		// Reverse unlocking a certain amount of stake to restake unlock
 		#[weight = 0]
 		fn lock_stake(origin, amount: BalanceOf<T>, stakee: T::AccountId) {
 			let staker = ensure_signed(origin)?;
 			let key = Self::get_key(staker, stakee);
+
+			Self::pull_unlocking(Some(key), amount);
+			Self::update_stake_amount(Some(key), Some(key), amount, false);
 		}
 
-		// Stop the unlocking of a certain amount of funds
-		// #[weight = 0]
-		// fn pull_unlocking() {
-		// }
-
-		// #[weight = 0]
-		// fn add_stake(origin, amount: BalanceOf<T>) {
-		// 	ensure!(amount != (0 as u32).into(), Error::<T>::ZeroStake);
-		// 	let address = ensure_signed(origin)?;
-
-		// 	// New stake
-		// 	if !<Stakes<T>>::contains_key(&address) {
-		// 		let parent = Some(<Root<T>>::get());
-		// 		let current = <Stakes<T>>::get(&address);
-		// 		while parent.is_some() {
-		// 			let next;
-		// 			if current.left_amount < current.right_amount {
-		// 				next = current.left;
-		// 			}
-		// 			else {
-		// 				next = current.right;
-		// 			}
-
-		// 			if(next.is_none()) {
-		// 				break;
-		// 			}
-
-		// 			parent = next;
-		// 			current = <Stakes<T>>::get(&address);
-
-		// 		}
-		// 	}
-		// }
-
-		//fn unlockStake(origin, amount: BalanceOf<T>, )
-
-
-
-
-		// #[weight = 0]
-		// fn add_stake(origin, amount: BalanceOf<T>) {
-		// 	let address = ensure_signed(origin)?;
-		// 	// Check that the node is not already staking
-		// 	ensure!(!<Stakes<T>>::contains_key(&address), Error::<T>::AlreadyStaking);
-
-		// 	//Remove funds from account
-			// T::Currency::withdraw(&address, amount, WithdrawReason::Fee.into(), ExistenceRequirement::KeepAlive)?;
-
-		// 	// Calculate lock period and create stake
-		// 	let unlock_at = T::Time::now() + UNLOCK_DURATION.into();
-		// 	let stake = Stake {amount, unlock_at};
-		// 	<Stakes<T>>::insert(address, stake);
-		// }
-
-		// #[weight = 0]
-		// fn get_stake(origin) {
-        //     let address = ensure_signed(origin)?;
-		// 	ensure!(<Stakes<T>>::contains_key(&address), Error::<T>::NotStaking);
-        //     <Stakes<T>>::get(address);
-		// }
-
-		// #[weight = 0]
-		// fn unstake(origin) {
-		// 	let address = ensure_signed(origin)?;
-		// 	ensure!(<Stakes<T>>::contains_key(&address), Error::<T>::NotStaking);
-		// 	let Stake {amount, unlock_at} = <Stakes<T>>::get(&address);
-		// 	ensure!(T::Time::now() >= unlock_at, Error::<T>::CannotUnlock); // Ensure unlock period exhausted
-		// 	T::Currency::deposit_into_existing(&address, amount);
-		// 	<Stakes<T>>::remove(&address);
-		// }
+		// Assumes that point is some random u128
 	}
 }
 
 impl<T: Trait> Module<T> {
-	fn get_key(staker: T::AccountId, stakee: T::AccountId) -> [u8; 32] {
+	fn get_key(staker: T::AccountId, stakee: T::AccountId) -> Hash {
 		let mut vec_bytes = staker.encode();
 		vec_bytes.append(&mut stakee.encode());
 		keccak_256(vec_bytes.as_slice())
 	}
 
-	fn set_child(stake: &mut Stake<T::AccountId, BalanceOf<T>>, old_key : Option<[u8; 32]>, new_key: Option<[u8; 32]>) {
+	fn set_child(key: Option<Hash>, old_key : Option<Hash>, new_key: Option<Hash>) {
+
+		let checked_key = match key {
+			Some(x) => x,
+			None => return,
+		};
+
+		let mut stake = <Stakes<T>>::get(checked_key);
 		if stake.left == old_key {
 			stake.left = new_key;
 		} else {
 			stake.right = new_key
 		}
+
+		<Stakes<T>>::insert(checked_key, stake);
 	}
 
-	fn update_stake_amount(key: Option<[u8; 32]>, stake: &mut Stake<T::AccountId, BalanceOf<T>>, amount: BalanceOf<T>) {
-		stake.amount += amount;
-		<Stakees<T>>::insert(&stake.stakee, amount);
+	fn update_stake_amount(key: Option<Hash>, stake_key: Option<Hash>, amount: BalanceOf<T>, flag: bool) {
 
-		Self::apply_stake_change(key, stake, amount, None);
+		let checked_stake_key = match stake_key {
+			Some(x) => x,
+			None => return,
+		};
+
+		let mut stake = <Stakes<T>>::get(checked_stake_key);
+
+		// XXX: Edit stake amount
+		if !flag {
+			stake.amount += amount;
+		} else {
+			stake.amount -= amount;
+		}
+
+		// Insert stakee amount into tree and update stake
+		<Stakes<T>>::insert(checked_stake_key, &stake);
+		<Stakees<T>>::insert(stake.stakee, amount);
+
+		// Apply edit stake amount to tree
+		Self::apply_stake_change(key, stake_key, amount, None);
 	}
 
 
-	fn apply_stake_change(key: Option<[u8; 32]>, stake: &mut Stake<T::AccountId, BalanceOf<T>>, amount: BalanceOf<T>, root_: Option<[u8; 32]>) {
-		let parent_key: Option<[u8; 32]> = stake.parent;
-		if parent_key == root_ || parent_key == None {
+	fn apply_stake_change(key: Option<Hash>, stake_key: Option<Hash>, amount: BalanceOf<T>, root_: Option<Hash>) {
+
+		let checked_stake_key = match stake_key {
+			Some(x) => x,
+			None => return,
+		};
+
+		let mut stake = <Stakes<T>>::get(checked_stake_key);
+
+		let parent_key = stake.parent;
+		let checked_parent_key = match parent_key {
+			Some(key) => key,
+			None => return,
+		};
+
+		if parent_key == root_ { // Option is deep equals
 			// we are at root, there is nothing left to update
 			return
 		}
 
-		let mut parent: Stake<T::AccountId, BalanceOf<T>> = <Stakes<T>>::get(parent_key.unwrap());
+		let mut parent = <Stakes<T>>::get(checked_parent_key);
 		if parent.left == key {
 			parent.left_amount += amount;
 		} else {
-			parent.right_amount+= amount;
+			parent.right_amount += amount;
 		}
 
-		return Self::apply_stake_change(parent_key, &mut parent, amount, root_);
+		<Stakes<T>>::insert(checked_parent_key, parent);
+
+		return Self::apply_stake_change(parent_key, parent_key, amount, root_);
 	}
 
-	fn fixl(stake: &Stake<T::AccountId, BalanceOf<T>>, current_key: Option<[u8; 32]>, current: &mut Stake<T::AccountId, BalanceOf<T>>) {
+
+
+	/// TODO make these use hashes
+
+
+
+	fn fixl(stake_key: Option<Hash>, current_key: Option<Hash>, current: Option<Hash>) {
+
+		// check the stake key
+		let checked_stake_key = match stake_key {
+			Some(x) => x,
+			None => return,
+		};
+
+		// fetch the stake
+		let stake = <Stakes<T>>::get(checked_stake_key);
+
 		if stake.left.is_none() {
 			return;
 		}
 
-		<Stakes<T>>::get(stake.left.unwrap()).parent = current_key;
-		current.left= stake.left;
+		let mut stake_left = <Stakes<T>>::get(stake.left.unwrap());
+		stake_left.parent = current_key;
+
+		// update current
+		let checked_current_key = match current_key {
+			Some(x) => x,
+			None => return,
+		};
+
+		let mut current = <Stakes<T>>::get(checked_current_key);
+
+		current.left = stake.left;
 		current.left_amount = stake.left_amount;
+
+		// update k/v
+		<Stakes<T>>::insert(checked_current_key, current);
+		<Stakes<T>>::insert(stake.left.unwrap(), stake_left);
 	}
 
-	fn fixr(stake: &Stake<T::AccountId, BalanceOf<T>>, current_key: Option<[u8; 32]>, current: &mut Stake<T::AccountId, BalanceOf<T>>) {
-		if stake.left.is_none() {
+	fn fixr(stake_key: Option<Hash>, current_key: Option<Hash>, current: Option<Hash>) {
+
+		// check the stake key
+		let checked_stake_key = match stake_key {
+			Some(x) => x,
+			None => return,
+		};
+
+		// fetch the stake
+		let stake = <Stakes<T>>::get(checked_stake_key);
+
+		if stake.right.is_none() {
 			return;
 		}
 
-		<Stakes<T>>::get(stake.right.unwrap()).parent = current_key;
-		current.right = stake.right;
+		let mut stake_right = <Stakes<T>>::get(stake.right.unwrap());
+		stake_right.parent = current_key;
+
+		// update current
+		let checked_current_key = match current_key {
+			Some(x) => x,
+			None => return,
+		};
+
+		let mut current = <Stakes<T>>::get(checked_current_key);
+
+		current.right = stake.right ;
 		current.right_amount = stake.right_amount;
+
+		// update k/v
+		<Stakes<T>>::insert(checked_current_key, current);
+		<Stakes<T>>::insert(stake.left.unwrap(), stake_right);
 	}
+
+	// todo: update error types
+	fn pull_unlocking(key: Option<Hash>, amount: BalanceOf<T>) -> Result<(), Error<T>> {
+		let checked_key = match key {
+			Some(x) => x,
+			_ => return Err(Error::<T>::UnlockPeriodNonExhasted),
+		};
+		let mut unlock = <Unlockings<T>>::get(checked_key);
+
+		// Lock period has not completed
+		if T::Time::now() < unlock.unlock_at {
+			return Err(Error::<T>::UnlockPeriodNonExhasted);
+		}
+
+		// Remove if locking whole unlock
+		if amount == unlock.amount {
+			<Unlockings<T>>::remove(checked_key);
+		} else {
+			ensure!(unlock.amount >= amount, Error::<T>::RelockAmount);
+			unlock.amount -= amount;
+			<Unlockings<T>>::insert(checked_key, unlock);
+		}
+		Ok(())
+	}
+
+
+	// Select a stake weighted node
+	// fn scan(point: U256) -> Hash {
+	// 	if Root::get() == [0u8; 32] {
+	// 		return [0u8; 32];
+	// 	}
+
+	// 	//let expectedValue = U256::from(Self::get_total_stake()) * point / U256::max_value();
+
+	// 	return [0u8; 32];
+	// }
+
+	// Retrieve the total stake weight
+	// fn get_total_stake() -> BalanceOf<T> {
+	// 	// empty root nothing staking
+	// 	if !Root::exists() {
+	// 		return (0 as u32).into();
+	// 	}
+	// 	else {
+	// 		let root_stake = <Stakes<T>>::get(Root::get());
+	// 		root_stake.amount + root_stake.left_amount + root_stake.right_amount
+	// 	}
+	// }
 }
