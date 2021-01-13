7bfa146798a615ae0b32e065b7d3c73f304b1bbc
wqsz7xn <wqsz7xn@protonmail.com>
2021-01-13 15:31:03 +1300

    Quick fix for Listing. Remove key parameter and use extrinsic source instead

--- crml/sylo/src/listing/mod.rs
+++ crml/sylo/src/listing/mod.rs
@@ -23,8 +23,8 @@ pub trait Trait: frame_system::Trait {
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin, system = frame_system {
 		#[weight = T::WeightInfo::set_listing()]
-		fn set_listing(origin, key: T::AccountId, multiaddr: Vec<u8>) {
-			ensure_signed(origin)?;
+		fn set_listing(origin, multiaddr: Vec<u8>) {
+			let key = ensure_signed(origin)?;
 			<Listings<T>>::insert(key, multiaddr);
 		}
 	}
